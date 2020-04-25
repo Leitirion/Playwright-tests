5371522ec57870f891f87681ff459bc0493581b6
Ilnar Gilyaev <gilyaev.if@gmail.com>
2020-04-25 17:18:42 +0500

    fix: some fix for test

--- test/test.js
+++ test/test.js
@@ -8,7 +8,6 @@ const playwright = require('playwright');
         await page.goto('http://ya.ru');
         await page.type('div[class="search2__input"]', `Hello ${browserType}!`, { delay: 100 });
         await page.screenshot({ path: `screenshots/test-${browserType}-Hello.png` });
-        await page.waitFor(5000);
         await page.click('button[class="button suggest2-form__button button_theme_websearch button_size_xl i-bem"]');
         await page.waitFor(5000);
         await page.screenshot({ path: `screenshots/test-${browserType}.png` });
