a3af2fcb7a5498ce9f941f337fd3655b1acadd3c
Ilnar Gilyaev <gilyaev.if@gmail.com>
2020-04-27 22:01:27 +0500

    feat: add env to test 3

--- test/test.js
+++ test/test.js
@@ -1,12 +1,12 @@
 const playwright = require('playwright');
 require('dotenv').config()
-const url = process.env.URL;
+const URL = process.env.URL;
 (async () => {
     for (const browserType of ['chromium', 'webkit']) {
         const browser = await playwright[browserType].launch();
         const context = await browser.newContext();
         const page = await context.newPage();
-        await page.goto(`${url}`);
+        await page.goto(`${URL}`);
         //await page.type('span[class="input__box"]', `Hello ${browserType}!`);
         //await page.click('button[class="button suggest2-form__button button_theme_websearch button_size_xl i-bem"]');
         //await page.waitFor(5000);
