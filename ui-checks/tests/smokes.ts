d978d15e11bf2e9216978f2d2d2540f0a4c32f31
Ilnar Gilyaev <gilyaev.if@gmail.com>
2022-01-09 00:39:58 +0500

    added beforeEach

--- ui-checks/tests/smokes.ts
+++ ui-checks/tests/smokes.ts
@@ -5,39 +5,36 @@ import { BasePage } from "../pages/base-page";
 test.describe("Smokes tests for kafka-ui", () => {
   const base = new BaseClass();
   test.base;
-  test("go to main page page and check title", async ({ page }) => {
+  test.beforeEach(async ({ page }) => {
     const mainPage = new BasePage(page);
     await mainPage.goto();
+  });
+  test("go to main page page and check title", async ({ page }) => {
+    const mainPage = new BasePage(page);
     await mainPage.titleIsOnPage();
   });
   test("go to main page page and check nav button text", async ({ page }) => {
     const mainPage = new BasePage(page);
-    await mainPage.goto();
     await mainPage.navButtonTextIsOnPage();
   });
   test("go to main page page and check Online title", async ({ page }) => {
     const mainPage = new BasePage(page);
-    await mainPage.goto();
     await mainPage.OnlineTitleIsOnPage();
   });
   test("go to main page page and check Offline title", async ({ page }) => {
     const mainPage = new BasePage(page);
-    await mainPage.goto();
     await mainPage.OfflineTitleIsOnPage();
   });
   test("go to main page page and check dashboard menuitem on page", async ({
     page,
   }) => {
     const mainPage = new BasePage(page);
-    await mainPage.goto();
     await mainPage.menuBarDashboardIsOnPage();
   });
-  //Only offline clusters
   test("go to main page page and check checkbox Only offline clusters", async ({
     page,
   }) => {
     const mainPage = new BasePage(page);
-    await mainPage.goto();
     await mainPage.checkboxIsOnPage();
   });
 });
