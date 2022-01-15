28a31963fa7d0c5f0ff258712225ba8fc8c49c18
Ilnar Gilyaev <gilyaev.if@gmail.com>
2022-01-16 01:09:10 +0500

    some fixes for tests

--- tests/api-tests/topics.ts
+++ tests/api-tests/topics.ts
@@ -1,7 +1,5 @@
 import { test, expect } from "@playwright/test";
-import { Topics } from "../../pages/topics";
 
-let topic: string;
 const topicName = "new.topic";
 const url = "http://localhost:8080/api/clusters/local/topics";
 test.describe("api tests for dashboard page", () => {
@@ -9,42 +7,41 @@ test.describe("api tests for dashboard page", () => {
   test("Create topic in topics page and get API response", async ({
     request,
   }) => {
-    const response = await request.post(`${url}`, {
+    const newResponse = await request.post(`${url}`, {
       data: {
-        name: `${topicName}`,
+        name: topicName,
         partitions: 1,
         replicationFactor: 1,
         configs: {},
       },
     });
-    expect(response.ok()).toBeTruthy();
-    console.log(await response.json());
-    const res = await response.json;
-    topic = res.name;
+    expect(await newResponse.ok()).toBeTruthy();
+    console.log(await newResponse.json());
+    //Get
   });
   //Get
-  test("get API response after creating new topic", async ({ request }) => {
-    const response = await request.get(`${url}`, {
-      params: {
-        name: topic,
-      },
-    });
-    expect(await response.ok()).toBeTruthy();
+  test("Use get request to check topic's name", async ({ request }) => {
+    const response = await request.get(`${url}/${topicName}`);
+    expect(response.ok()).toBeTruthy();
+    expect((await response.json()).name).toEqual(topicName);
     console.log(await response.json());
   });
   //Update
-  test("Patch(Modify) topic", async ({ request }) => {
-    const response = await request.patch(`${url}/${topicName}`, {
-      data: {
-        cleanUpPolicy: "DELETE",
-      },
-    });
-    console.log(await response.json());
-    expect(response.ok()).toBeTruthy();
+//   test("Patch(Modify) topic parameters", async ({ request }) => {
+//     const response = await request.patch(`${url}/${topicName}`, {
+//       data: {
+//         name: topicName,
+//         partitions: 1,
+//         replicationFactor: 1,
+//         configs: {},
+//       },
+//     });
+//     expect(await response.ok()).toBeTruthy();
+//     console.log(await response.json());
   });
   //   Delete;
   test("Delete topic", async ({ request }) => {
     const response = await request.delete(`${url}/${topicName}`);
-    expect(response.ok()).toBeTruthy();
+    expect(await response.ok()).toBeTruthy();
   });
 });
