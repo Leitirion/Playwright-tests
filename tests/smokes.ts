2bf97ba8f9462f8615722a771921970a912caf95
Ilnar Gilyaev <gilyaev.if@gmail.com>
2022-01-16 02:01:22 +0500

    some fixes for tests

--- tests/smokes.ts
+++ tests/smokes.ts
@@ -1,4 +1,4 @@
-import { test, expect } from "@playwright/test";
+import { test } from "@playwright/test";
 import { BasePage } from "../pages/base-page";
 
 test.describe("Smokes tests for kafka-ui", () => {
