17b8258b70efba0a6b6744591918baf53de91e2b
Ilnar Gilyaev <gilyaev.if@gmail.com>
2022-01-16 01:57:57 +0500

    some fixes for tests

--- tests/api-tests/dashboards.ts
+++ tests/api-tests/dashboards.ts
@@ -1,14 +1,16 @@
-import { test, expect } from "@playwright/test";
+import { test } from "@playwright/test";
 import { BasePage } from "../../pages/base-page";
 
-test.describe("api test for dashboard page", () => {
-  test("Reload main page and get API response", async ({ page }) => {
+test.describe("api tests for dashboard page", () => {
+  test.beforeEach(async ({ page }) => {
     const mainPage = new BasePage(page);
     await mainPage.goto();
+  });
+  test("Reload main page and get API response", async ({ page }) => {
     const response = await Promise.all([
       page.click('//*[@id="root"]//nav//a[text()="UI for Apache Kafka"]'),
+      page.waitForResponse((res) => res.status() == 200),
     ]);
-    expect(response.ok()).toBeTruthy();
-    console.log(await JSON.stringify(response));
+    console.log(JSON.stringify(response, null, "\t"));
   });
 });
